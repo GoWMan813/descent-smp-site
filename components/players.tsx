02f792ca0658f0fced6c70ed88d3cf04909b0932
GodOfWonders <33267417+GodOfWonders@users.noreply.github.com>
2022-12-17 15:15:52 -0600

    waldo deaths

--- components/players.tsx
+++ components/players.tsx
@@ -59,7 +59,7 @@ export const playercols = [
         role: "Player",
         title: "Friends Still Looking for Him",
         status: "Active",
-        deaths: "24",
+        deaths: "26",
     },
     {
         key: "6",
