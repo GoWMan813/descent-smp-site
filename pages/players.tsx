289390bc18bf2b5e77706f575752926bef57af08
KK <morrkade05@gmail.com>
2022-12-17 19:12:44 -0600

    Added death api endpoint

--- pages/players.tsx
+++ pages/players.tsx
@@ -1,9 +1,29 @@
 import Head from 'next/head'
+import { Table } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
+
 import styles from '../styles/Home.module.css'
-import { Table } from '@nextui-org/react';
-import { playercols, playerrows } from '../components/players';
+import { playercols, playerrows } from '../components/players'
+
+type Death = {
+  name: string;
+  deaths: number;
+  causes: string[];
+};
 
 export default function Players() {
+  const [deaths, setDeaths] = useState<Death[] | undefined>();
+
+  async function fetchDeaths() {
+    const resp = await fetch('/api/deaths');
+    const data = await resp.json();
+    setDeaths(data.data);
+  }
+
+  useEffect(() => {
+    fetchDeaths();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -24,12 +44,19 @@ export default function Players() {
           <Table.Header columns={playercols}>
             {(playercol) => <Table.Column key={playercol.key}>{playercol.label}</Table.Column>}
           </Table.Header>
-          <Table.Body items={playerrows}>
-            {(item) => (
-              <Table.Row key={item.key}>
-                {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
-              </Table.Row>
-            )}
+          <Table.Body items={!deaths ? [] : playerrows}>
+            {(item) => {
+              const death = deaths.find((death) => death.name === item.name);
+              if (death) {
+                item.deaths = death.deaths.toString();
+              }
+
+              return (
+                <Table.Row key={item.key}>
+                  {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+                </Table.Row>
+              );
+            }}
           </Table.Body>
         </Table>
       </main>
